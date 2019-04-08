c9373ce4d2d17fc7a0cf3dfac178f32ca7e62903
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-08 00:27:41 +0000

    Snapshot of branch master taken on 2019-04-07T20:27:40.395-04:00 from "https://github.com/UnofficialJuliaMirror/Mosek.jl-6405355b-0ac2-5fba-af84-adbd65488c0e"

---
+++ test/testexamples.jl
@@ -0,0 +1,39 @@
+@testset "[distro examples]" begin
+    @testset "lo1" begin
+        include("../examples/lo1.jl")
+    end
+    @testset "cqo1" begin
+        include("../examples/cqo1.jl")
+    end
+    @testset "qo1" begin
+        include("../examples/qo1.jl")
+    end
+    @testset "qcqo1" begin
+        include("../examples/qcqo1.jl")
+    end
+    @testset "milo1" begin
+        include("../examples/milo1.jl")
+    end
+    @testset "sdo1" begin
+        include("../examples/sdo1.jl")
+    end
+    @testset "eo1" begin
+        include("../examples/eo1.jl")
+    end
+    @testset "pow1" begin
+        include("../examples/pow1.jl")
+    end
+    @testset "solvebasis" begin
+        include("../examples/solvebasis.jl")
+    end
+    @testset "portfolio" begin
+        include("../examples/portfolio.jl")
+    end
+    @testset "production" begin
+        include("../examples/production.jl")
+    end
+    @testset "callback" begin
+        include("../examples/callback.jl")
+
+    end
+end
